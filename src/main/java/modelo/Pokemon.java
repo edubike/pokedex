77b2c597966056d978718b864fd1109e7a1f9e47
edubike <69863170+edubike@users.noreply.github.com>
2021-01-17 19:24:54 -0600

    .

--- src/main/java/modelo/Pokemon.java
+++ src/main/java/modelo/Pokemon.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 public class Pokemon {
 
     @Id
-    @@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String nombre;
